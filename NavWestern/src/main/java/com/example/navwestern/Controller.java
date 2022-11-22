b5240e5e0e636c68731748bedb0913763333c326
Michael Salton <msalton@uwo.ca>
2022-11-22 15:56:31 -0500

    fixed main

--- NavWestern/src/main/java/com/example/navwestern/Controller.java
+++ NavWestern/src/main/java/com/example/navwestern/Controller.java
@@ -1,14 +1,27 @@
 package com.example.navwestern;
 
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ChoiceBox<String> selectBuildingBox;
+
+    private final String[] buildings = {"Middlesex College","University College","Amit Chakma Engineering Building"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        selectBuildingBox.getItems().addAll(buildings);
+
     }
 }
--- NavWestern/src/main/java/com/example/navwestern/Main.java
+++ NavWestern/src/main/java/com/example/navwestern/Main.java
@@ -2,6 +2,7 @@ package com.example.navwestern;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,11 +11,12 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
-        stage.setScene(new Scene);
+        Parent root = FXMLLoader.load(getClass().getResource("home_page.fxml"));
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setTitle("NavWestern");
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
