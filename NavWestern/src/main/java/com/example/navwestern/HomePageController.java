0a92025001661782ded88803fc3b6f9d41e22a57
Michael Salton <msalton@uwo.ca>
2022-11-29 21:05:05 -0500

    working image view

--- NavWestern/src/main/java/com/example/navwestern/HomePageController.java
+++ NavWestern/src/main/java/com/example/navwestern/HomePageController.java
@@ -9,11 +9,14 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class HomePageController implements Initializable{
+public class HomePageController implements Initializable {
     @FXML
     private Button homeButton, createPOIButton, devModeButton;
     @FXML
@@ -21,14 +24,17 @@ public class HomePageController implements Initializable{
     @FXML ChoiceBox<String> selectFloorBox;
     @FXML
     private ImageView imageView;
+    public HomePageController() throws FileNotFoundException {
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         selectBuildingBox.getItems().addAll("Middlesex College","University College","Amit Chakma Engineering Building");
         selectFloorBox.getItems().addAll("First Floor","Second Floor","Third Floor");
     }
     @FXML
-    public void getBuilding(MouseEvent event) {
+    public void getBuilding(MouseEvent event) throws FileNotFoundException {
         String building = selectBuildingBox.getSelectionModel().getSelectedItem();
+        displayImage();
     }
     public void getFloor(MouseEvent event) {
         String building = selectFloorBox.getSelectionModel().getSelectedItem();
@@ -39,14 +45,12 @@ public class HomePageController implements Initializable{
         Main m = new Main();
         m.changeScene("userCreatePOI.fxml");
     }
-
     public void DevModeButtonOnAction(ActionEvent e) throws IOException {
         Main m = new Main();
         m.changeScene("developer_login.fxml");
     }
-
-    public void displayImage() {
-
+    public void displayImage() throws FileNotFoundException {
+        Image image = new Image(getClass().getResourceAsStream("/img/MC1.png"));
+        imageView.setImage(image);
     }
 }
-
