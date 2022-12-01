603f5495a9ac85aa55929d459fb600c9e5892952
Michael Salton <msalton@uwo.ca>
2022-11-30 19:33:26 -0500

    fixed floor selction

--- NavWestern/src/main/java/com/example/navwestern/HomePageController.java
+++ NavWestern/src/main/java/com/example/navwestern/HomePageController.java
@@ -30,22 +30,28 @@ public class HomePageController implements Initializable {
     Image UC1 = new Image(getClass().getResourceAsStream("/img/UC1.png"));
     Image UC2 = new Image(getClass().getResourceAsStream("/img/UC2.png"));
     Image UC3 = new Image(getClass().getResourceAsStream("/img/UC3.png"));
-    Image UC4 = new Image(getClass().getResourceAsStream("/img/UC4.png"));
+    Image UC4 = new Image(getClass().getResourceAsStream("/img/UC1.png"));
+    Image ACEB1 = new Image(getClass().getResourceAsStream("/img/ACEB1.png"));
+    Image ACEB2 = new Image(getClass().getResourceAsStream("/img/ACEB2.png"));
+    Image ACEB3 = new Image(getClass().getResourceAsStream("/img/ACEB3.png"));
+    Image ACEB4 = new Image(getClass().getResourceAsStream("/img/ACEB4.png"));
+    Image ACEB5 = new Image(getClass().getResourceAsStream("/img/ACEB5.png"));
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         selectBuildingBox.getItems().addAll("Middlesex College","University College","Amit Chakma Engineering Building");
-        selectFloorBox.getItems().addAll("First Floor","Second Floor","Third Floor");
+        selectFloorBox.getItems().addAll("First Floor","Second Floor","Third Floor", "Fourth Floor", "Fifth Floor");
     }
-    @FXML
-    public void getBuilding(MouseEvent event) throws FileNotFoundException {
-        String building = selectBuildingBox.getSelectionModel().getSelectedItem();
+
+    public String getBuilding() {
+        return selectBuildingBox.getSelectionModel().getSelectedItem();
     }
-    public void getFloor(MouseEvent event) {
-        String floor = selectFloorBox.getSelectionModel().getSelectedItem();
+
+    public String getFloor() {
+        return selectFloorBox.getSelectionModel().getSelectedItem();
     }
     public void DisplayMapButtonOnAction(ActionEvent e) {
-        String building = selectBuildingBox.getSelectionModel().getSelectedItem();
-        String floor = selectFloorBox.getSelectionModel().getSelectedItem();
+        String building = getBuilding();
+        String floor = getFloor();
         switch (building) {
             case "Middlesex College":
                 switch (floor) {
@@ -65,6 +71,7 @@ public class HomePageController implements Initializable {
                         imageView.setImage(MC5);
                         break;
                 }
+            break;
             case "University College":
                 switch (floor) {
                     case "First Floor":
@@ -80,21 +87,26 @@ public class HomePageController implements Initializable {
                         imageView.setImage(UC4);
                         break;
                 }
+            break;
             case "Amit Chakma Engineering Building":
                 switch (floor) {
                     case "First Floor":
-                        imageView.setImage(UC1);
+                        imageView.setImage(ACEB1);
                         break;
                     case "Second Floor":
-                        imageView.setImage(UC2);
+                        imageView.setImage(ACEB2);
                         break;
                     case "Third Floor":
-                        imageView.setImage(UC3);
+                        imageView.setImage(ACEB3);
                         break;
                     case "Fourth Floor":
-                        imageView.setImage(UC4);
+                        imageView.setImage(ACEB4);
+                        break;
+                    case "Fifth Floor":
+                        imageView.setImage(ACEB5);
                         break;
                 }
+            break;
         }
     }
     public void HomeButtonOnAction(ActionEvent e) throws IOException {
